2ae05a18648aaeb60c2382e33ee46f5bd7342e44
rafael2901 <rafael.co@aluno.ifsc.edu.br>
2023-05-23 15:09:47 -0300

    Classe itensDePedido

--- src/br/com/Produto.java
+++ src/br/com/Produto.java
@@ -6,28 +6,52 @@ public class Produto {
 	private String descricao;
 	private int quantidade;
 	private double valor;
-	
-	
-	
-	
-	
-	
-	
-	
-	public String consultar() {
-		
-		
-		
-		
-		return "falta fazer aq";
+
+	public Long getCodigo() {
+		return codigo;
 	}
 
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
 
+	public String getDescricao() {
+		return descricao;
+	}
 
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
+	public int getQuantidade() {
+		return quantidade;
+	}
 
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
 
+	public double getValor() {
+		return valor;
+	}
 
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String consultar(int codigo) {
+
+		int codigoCadastrado = 0;
+		
+		if (codigo == codigoCadastrado) {
+			System.out.println("O nome desse produto é: " +getCodigo());
+		}
+		else {
+			System.out.println("Erro! código invalido.");
+		}
+
+		return " sla ";
+	}
 
 	public Produto(Long codigo, String descricao, int quantidade, double valor) {
 		super();
@@ -35,7 +59,7 @@ public class Produto {
 		this.descricao = descricao;
 		this.quantidade = quantidade;
 		this.valor = valor;
-		
+
 	}
-	
+
 }
--- src/br/com/itensDePedido.java
+++ src/br/com/itensDePedido.java
@@ -1,5 +1,38 @@
 package br.com;
 
 public class itensDePedido {
-
+   
+	private int quantidade;
+	private Produto produto;
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	public boolean incrementaQtde() {
+		
+		System.out.println("insira a quantidade que deseja desse produto" +getQuantidade());
+				
+		return false;
+		
+	}
+	
+	public boolean decrementaQtde() {
+		
+		System.out.println("Qual produto você deseja decrementar?" +getQuantidade());
+		
+		return false;
+		
+	}
+	
+	
 }
