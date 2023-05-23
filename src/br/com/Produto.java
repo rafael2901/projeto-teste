3e869fdc614bde2c5485d956ac55c8f8f08c7e9f
rafael2901 <rafael.co@aluno.ifsc.edu.br>
2023-05-23 14:42:33 -0300

    Classe Produto

--- src/br/com/Produto.java
+++ src/br/com/Produto.java
@@ -2,4 +2,40 @@ package br.com;
 
 public class Produto {
 
+	private Long codigo;
+	private String descricao;
+	private int quantidade;
+	private double valor;
+	
+	
+	
+	
+	
+	
+	
+	
+	public String consultar() {
+		
+		
+		
+		
+		return "falta fazer aq";
+	}
+
+
+
+
+
+
+
+
+	public Produto(Long codigo, String descricao, int quantidade, double valor) {
+		super();
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.quantidade = quantidade;
+		this.valor = valor;
+		
+	}
+	
 }
